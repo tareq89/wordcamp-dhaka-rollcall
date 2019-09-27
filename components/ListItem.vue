1880e91878307a76242a4eecee063f72bb54d83d
Foysal Zafree <zafree@deligram.com>
2019-09-27 20:01:50 +0600

    update json data

--- components/ListItem.vue
+++ components/ListItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div :class="['VolunteerList__item', {'VolunteerList__item--active': item.active}, {'VolunteerList__item--unknown': !item.known}]">
     <div class="VolunteerList__imageWrapper">
-      <img src="~/assets/images/Saif_Hassan.jpg" :alt="item.name">
+      <img :src="`${item.image}`" :alt="item.name">
     </div>
     <div class="VolunteerList__copy">
       <a class="VolunteerList__name" :href="`tel:+${item.phoneNumber}`">{{ item.name }}</a>
